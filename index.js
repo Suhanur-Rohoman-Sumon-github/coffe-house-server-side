989af3a2a3be235101907a44f21b886cd7d65d9c
su44248124 <sumonsuhanurrohoman@gmail.com>
2023-05-10 13:58:20 +0600

    .env file added

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+const uri = "mongodb+srv://coffiehouse:iBdxlFLJvLdqxM48@cluster0.eepi0pq.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send('vai kaj running coltese pera nio na')
+})
+
+app.listen(port,()=>{
+    console.log(`coffie is running in port ${port}`)
+})
+
